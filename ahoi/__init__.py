c988494471827fc3becf82738effc14520472930
Nikolai Hartmann <nikoladze@posteo.de>
2020-02-08 18:32:05 +0100

    reduce overhead in PerEventScannerC

--- ahoi/__init__.py
+++ ahoi/__init__.py
@@ -117,7 +117,9 @@ class PerEventScanner(Scanner):
         )
 
     def run(self, progress=True):
-        for i in tqdm(range(len(self.masks_list[0][0])), disable=not progress, desc="Events"):
+        for i in tqdm(
+            range(len(self.masks_list[0][0])), disable=not progress, desc="Events"
+        ):
 
             # fill per event buffer
             for i_mask, masks in enumerate(self.masks_list):
@@ -140,9 +142,11 @@ class PerEventScannerC(PerEventScanner):
         # ... not sure if this is the right way to find the library
         if sys.version_info[0] < 3:
             import pkgutil
+
             lib_filename = pkgutil.get_loader("ahoi_scan").filename
         else:
             import importlib
+
             lib_filename = importlib.util.find_spec("ahoi_scan").origin
         lib = ctypes.cdll.LoadLibrary(lib_filename)
         self._fill_matching = lib.fill_matching
@@ -160,35 +164,37 @@ class PerEventScannerC(PerEventScanner):
             ctypes.c_bool,  # use_weights
         ]
 
+        # prepare array of pointers for 2D per-event masks buffer
+        self._p_masks = np.array(
+            self.masks_buffer.__array_interface__["data"][0]
+            + (
+                np.arange(self.masks_buffer.shape[0]) * self.masks_buffer.strides[0]
+            ).astype(np.uintp)
+        )
+        # the other pointers
+        self._p_multi_index = np.zeros_like(self.shape, dtype=np.int32)
+        self._p_counts = self.counts.ravel()
+        self._p_sumw = np.empty(0) if self.sumw is None else self.sumw.ravel()
+        self._p_sumw2 = np.empty(0) if self.sumw2 is None else self.sumw2.ravel()
+        self._p_shape = self.shape.astype(np.int32)
+
     def run_event(self, masks_buffer, w=None):
         "Wrap around c function"
 
-        # prepare array of pointers for 2D per-event masks buffer
-        p_masks = np.array(
-            masks_buffer.__array_interface__["data"][0]
-            + (np.arange(masks_buffer.shape[0]) * masks_buffer.strides[0]).astype(
-                np.uintp
-            )
-        )
-
-        counts = self.counts.ravel()
         use_weights = w is not None
-        sumw = np.empty(0) if self.sumw is None else self.sumw.ravel()
-        sumw2 = np.empty(0) if self.sumw2 is None else self.sumw2.ravel()
         if w is None:
             w = 0
-        multi_index = np.zeros_like(self.shape, dtype=np.int32)
 
         self._fill_matching(
-            p_masks,
+            self._p_masks,
             w,
             0,
-            multi_index,
-            self.shape.astype(np.int32),
-            self.shape.size,
-            counts,
-            sumw,
-            sumw2,
+            self._p_multi_index,
+            self._p_shape,
+            self._p_shape.size,
+            self._p_counts,
+            self._p_sumw,
+            self._p_sumw2,
             use_weights,
         )
 
@@ -217,9 +223,10 @@ class ScannerNumpy(Scanner):
                     yield 1
 
         for i in tqdm(
-                fill(0, current_mask), total=len(self.counts.ravel()),
-                desc="Combinations",
-                disable=not progress
+            fill(0, current_mask),
+            total=len(self.counts.ravel()),
+            desc="Combinations",
+            disable=not progress,
         ):
             pass
 
@@ -244,7 +251,8 @@ class ScannerNumpyReduce(Scanner):
                     new_w2 = w2[mask]
                 if j != (len(self.masks_list) - 1):
                     new_masks_list = [
-                        [new_mask[mask] for new_mask in masks] for masks in masks_list[1:]
+                        [new_mask[mask] for new_mask in masks]
+                        for masks in masks_list[1:]
                     ]
                     for _ in fill(new_masks_list, j + 1, new_w, new_w2):
                         yield 1
@@ -256,8 +264,9 @@ class ScannerNumpyReduce(Scanner):
                     yield 1
 
         for i in tqdm(
-                fill(self.masks_list, 0, w, w2), total=len(self.counts.ravel()),
-                desc="Combinations",
-                disable=not progress
+            fill(self.masks_list, 0, w, w2),
+            total=len(self.counts.ravel()),
+            desc="Combinations",
+            disable=not progress,
         ):
             pass
